82eff9cbfc10dc823c9db145173a66ad53415993
Cheb <chebts@gmail.com>
2023-04-27 01:11:12 +0300

    first commit

---
+++ lib/cli2.dart
@@ -0,0 +1,25 @@
+
+import 'package:http/http.dart' as http;
+
+import 'dart:convert' as convert;
+
+int calculate() {
+  return 6 * 7;
+}
+
+void getData() async{
+  final url = Uri.https('api.genderize.io', '/', {'name': 'lena'});
+
+  // Await the HTTP GET response, then decode the
+  // JSON data it contains.
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final jsonResponse = convert.jsonDecode(response.body);
+    print(jsonResponse);
+    //     final itemCount = jsonResponse['totalItems'];
+    //     print('Number of books about HTTP: $itemCount.');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
+}
---
+++ test/cli2_test.dart
@@ -0,0 +1,8 @@
+import 'package:cli2/cli2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
